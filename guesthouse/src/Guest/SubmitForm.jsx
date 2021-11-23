79dcebd7dcfc2bcc42e5e2ffec0ff2eb01fa99dd
pearlkothari <pearlkothari2@gmail.com>
2021-11-23 16:25:57 +0530

    commit to master

--- guesthouse/src/Guest/SubmitForm.jsx
+++ guesthouse/src/Guest/SubmitForm.jsx
@@ -3,35 +3,7 @@ import React from 'react'
 function SubmitForm() {
     return (
         <div className="submit">
-            <div className="Form">
-                <p>1.Name</p>
-                <input type="text"></input>
-                <p>Roll No./Designation</p>
-                <input type="text"></input>
-            </div>
-            <div className="Form">
-                <p>2.Branch/Department</p>
-                <input type="text"></input>
-                <p>Mobile No.</p>
-                <input type="text"></input>
-            </div>
-            <div className="Form">
-                <p>3.Guest Name</p>
-                <input type="text"></input>
-                <p>Relation</p>
-                <input type="text"></input>
-            </div>
-            <div className="Form">
-                <p>4. Guest Mobile No.</p>
-                <input type="text"></input>
-            </div>
-            <div className="Form">
-                <p>5.	No. of Persons</p>
-                <p>Male</p>
-                <input type="text"></input>
-                <p>Female</p>
-                <input type="text"></input>
-            </div>
+            
         </div>
     )
 }
