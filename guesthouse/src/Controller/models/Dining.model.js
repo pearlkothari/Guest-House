21e4a03eb31934c2b9efa7bddd93c57fa3439fb0
pearlkothari <pearlkothari2@gmail.com>
2021-12-24 13:55:45 +0530

    commit to master

--- guesthouse/src/Controller/models/Dining.model.js
+++ guesthouse/src/Controller/models/Dining.model.js
@@ -5,6 +5,7 @@ const dininSchema = new Schema({
     emailId: {type: String, required: true, default: false},
     reservationDate: {type: Date, required: true, default: false},
     totalGuests: {type: Number, required: true, default: false},
+    approved: {type: Boolean, required: true, default: false},
 })
 const dinin = mongoose.model('Dinin', dininSchema);
 module.exports = dinin;
